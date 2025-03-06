b96d997cb607e8f6f4702050f3b51e533acda52f
Sants <106097059+CardonaSantos@users.noreply.github.com>
2025-03-06 14:00:50 -0600

    ajustes-db-2

--- prisma/generated/pos/index.js
+++ prisma/generated/pos/index.js
@@ -825,7 +825,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     // origin: 'https://pos-ui-production.up.railway.app', // Cambia esta línea para permitir solo tu frontend
     // origin: 'http://localhost:5173', // Cambia esta línea para permitir solo tu frontend
     credentials: true,
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   });
 
